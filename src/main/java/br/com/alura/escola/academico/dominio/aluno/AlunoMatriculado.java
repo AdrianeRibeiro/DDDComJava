d2e3290bbc33bbe9330d77db69fe902fdfe6430e
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2021-07-19 19:48:15 -0300

    ddd com java

---
+++ src/main/java/br/com/alura/escola/academico/dominio/aluno/AlunoMatriculado.java
@@ -0,0 +1,37 @@
+package br.com.alura.escola.academico.dominio.aluno;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import br.com.alura.escola.shared.dominio.CPF;
+import br.com.alura.escola.shared.dominio.evento.Evento;
+import br.com.alura.escola.shared.dominio.evento.TipoDeEvento;
+
+public class AlunoMatriculado implements Evento {
+	
+	private final CPF cpfDoAluno;
+	private final LocalDateTime momento;
+
+	public AlunoMatriculado(CPF cpfDoAluno) {
+		this.cpfDoAluno = cpfDoAluno;
+		this.momento = LocalDateTime.now();
+	}
+
+	public LocalDateTime momento() {
+		return this.momento();
+	}
+
+	public CPF getCpfDoAluno() {
+		return cpfDoAluno;
+	}
+
+	public TipoDeEvento tipo() {
+		return TipoDeEvento.ALUNO_MATRICULADO;
+	}
+
+	@Override
+	public Map<String, Object> informacoes() {
+		// TODO Auto-generated method stub
+		return Map.of("cpf", cpfDoAluno);
+	}
+}
---
+++ src/test/java/br/com/alura/escola/ademico/infra/selo/RepositorioDeSelosEmMemoriaTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.escola.ademico.infra.selo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RepositorioDeSelosEmMemoriaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
